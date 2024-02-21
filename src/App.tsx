7a05fb8ac9eefbf78f552743d8bae7b93a6680fc
Samyak Luitel <samkotaku@gmail.com>
2024-02-21 22:16:29 +0000

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { AppContainer } from './styles';
+
+function App() {
+  return (
+    <AppContainer>
+      trello app
+    </AppContainer>
+  );
+}
+
+export default App;
